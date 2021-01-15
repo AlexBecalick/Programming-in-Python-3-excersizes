d7583d05dd934b94dd73dfa27cad7b9ca9061846
AlexBecalick <alexander.becalick@crick.ac.uk>
2021-01-15 15:21:32 +0000

    Completed exercises in chapter 1

---
+++ Chapter 1/average.py
@@ -0,0 +1,55 @@
+#Defining object references
+numbers = []
+sum = 0
+count = 0
+lowest = 0
+highest = 0
+median = 0
+
+#A loop for iterating requests for integer inputs and determining highest/lowest values
+while True:
+    char = input("Type integers followed by enter, or just enter to finish: ")
+    if char:
+        try:
+            number = int(char)
+        except ValueError as err:
+            print(err)
+            continue
+        sum += number
+        if count == 0:
+            lowest = number
+        else:
+            if number < lowest:
+                lowest = number
+        if count == 0:
+            highest = number
+        else:
+            if number > highest:
+                highest = number
+        numbers.append(number)
+        count +=1
+    else:
+        break
+
+if count:
+#A loop sorting the list of inputted numbers from lowest to highest
+    sorted = []
+    while numbers:
+        smallest = numbers[0]
+        for number in numbers:
+            if number < smallest:
+                smallest = number
+        sorted.append(smallest)
+        numbers.remove(smallest)
+
+    #A calculation of the median from the sorted number list
+    if len(sorted)%2:
+        median = sorted[(len(sorted)-1)//2]
+    else:
+        median = (sorted[(len(sorted)//2)] + sorted[(len(sorted)//2)+1]) / 2
+    
+    #Output and calculation
+    print(" sorted = ", sorted)
+    print("count = ", count, " sum = ", sum, "lowest = ", lowest,
+        "highest = ", highest, " mean = ", sum/count, " median = ",
+        median)
--- Chapter 1/bigdigits.py
+++ Chapter 1/bigdigits.py
@@ -1,5 +1,6 @@
 import sys
 
+#Defining big numbers as lists containing characters split into rows
 Zero  = [" *** ", 
          "*   *", 
          "*   *", 
@@ -73,6 +74,9 @@ Nine  = [" ****",
 
 Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
 
+#A loop which takes command line integer input and converts it to big digits
+#For each character in a column, the corresponding input number is checked and 
+#assigned in place of all the placeholder * characters
 try:
     digits = sys.argv[1]
     row = 0
@@ -82,7 +86,12 @@ try:
         while column < len(digits):
             number = int(digits[column])
             digit = Digits[number]
-            line += digit[row] + " "
+            for character in digit[row]:
+                if character == "*":
+                    line += str(number)
+                else:
+                    line += character
+            line += " "
             column += 1
         print(line)
         row += 1
